480eab93d4a596f1ae4b53621797148627b1cfa5
tombunzel <bunzel.tom@gmail.com>
2026-01-15 21:32:54 +0100

    feat(ports): add driving and driven ports

---
+++ src/ports/driven/TaxRateRepository.ts
@@ -0,0 +1,3 @@
+export interface TaxRateRepository {
+  getRate(): number;
+}
---
+++ src/ports/driving/TaxCalculatorPort.ts
@@ -0,0 +1,4 @@
+// This is the "contract". Any UI or Test must follow this.
+export interface TaxCalculatorPort {
+  calculateTax(amount: number): number;
+}
