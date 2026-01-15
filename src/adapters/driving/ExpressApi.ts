0a3119da2575f8da7bd5499ee6e03f82db0353b1
tombunzel <bunzel.tom@gmail.com>
2026-01-15 22:46:34 +0100

    feat(adapter): impmlement ExpressApi as a REST driving adapter

---
+++ src/adapters/driving/ExpressApi.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from "express";
+import { TaxCalculatorPort } from "../../ports/driving/TaxCalculatorPort";
+import { parse } from "node:path";
+
+export class ExpressApi {
+  private app = express();
+
+  constructor(private taxCalculator: TaxCalculatorPort) {
+    // Middleware to parse JSON bodies
+    this.app.use(express.json());
+
+    // Define the Route
+    this.app.get("/tax", (req: Request, res: Response) => {
+      // 1. Adapt Input: Extract data from Query Params
+      const amount = parseFloat(req.query.amount as string);
+      const discount = parseFloat(req.query.discount as string) || 0;
+
+      if (isNaN(amount)) {
+        res.status(400).send({ error: "Invalid amount" });
+        return;
+      }
+
+      // 2. Call the Port (The Core)
+      const tax = this.taxCalculator.calculateTax(amount, discount);
+
+      // 3. Adapt Output: Return JSON Response
+      res.send({
+        amount,
+        discount,
+        tax,
+      });
+    });
+  }
+
+  start(port: number) {
+    this.app.listen(port, () => {
+      console.log(`Hexagonal Tax API listerning on http://localhost:${port}`);
+    });
+  }
+
+  public getApp() {
+    return this.app;
+  }
+}
