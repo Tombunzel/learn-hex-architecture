db6a31d2353ce778de2b608430c6729bdeaf9969
tombunzel <bunzel.tom@gmail.com>
2026-01-15 21:41:35 +0100

    test(TaxCalculator.test): adjust to test for both repos

--- src/adapters/driving/TaxCalculator.test.ts
+++ src/adapters/driving/TaxCalculator.test.ts
@@ -1,8 +1,9 @@
 import { TaxCalculator } from "../../core/TaxCalculator";
 import { StubTaxRateRepository } from "../driven/StubTaxRateRepository";
+import { JsonTaxRateRepository } from "../driven/JsonTaxRateRepository";
 
-describe("TaxCalculator Driving Adapter (Test)", () => {
-  it("should calculate tax using the rate from the driven adapter", () => {
+describe("TaxCalculator with Different Adapaters", () => {
+  it("works with the Stub Repository (20%)", () => {
     // 1. Arrange: Create the core
     const stubRepo = new StubTaxRateRepository();
     const calculator = new TaxCalculator(stubRepo);
@@ -12,5 +13,15 @@ describe("TaxCalculator Driving Adapter (Test)", () => {
 
     // 3. Assert: verify the behaviour
     expect(result).toBe(20);
+  });
+
+  it("works with the JSON Repository (15%)", () => {
+    // Thanks to Hex, we plug in the new adapter into the same core.
+    const JsonRepo = new JsonTaxRateRepository();
+    const calculator = new TaxCalculator(JsonRepo);
+
+    const result = calculator.calculateTax(100);
+
+    expect(result).toBe(15);
   });
 });
